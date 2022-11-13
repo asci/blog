a9808288a1fc9d579f6f43d5625de512970f7333
Artem <asci89@gmail.com>
2022-11-13 07:20:17 +0100

    adding mermaid for charts

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginMermaid);
 
   return {
     dir: {
