11a2ede2fd04c785e041226270ff33384c85d32a
Artem Riasnianskyi <asci89@gmail.com>
2025-02-09 21:48:25 +0100

    add calc app

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPlugin(pluginRss);
 
+  // Pass through the built React app files
+  eleventyConfig.addPassthroughCopy({ "src/apps/calc/dist": "apps/calc" });
+
   return {
     dir: {
       input: "src",
